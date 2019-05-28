daccdfc72707e46d7412828289f6acc91121d4d2
Jeff Oliver <jeffoliver3384@gmail.com>
2019-05-27 21:44:48 -0400

    WIP: Header components complete

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div>
+      <h1>Lambda School</h1>
+      <span>@Lambda School &bull; 26 jan</span>
+    </div>
+  );
+};
+
+export default HeaderTitle;
